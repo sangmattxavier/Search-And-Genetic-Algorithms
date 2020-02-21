4bdf4b3daa9c776e677c073fd396535daa1c5d8f
mwlee15 <MLeeW2015@gmail.com>
2020-02-21 02:34:21 -0500

    hill works

--- src/HillClimb.java
+++ src/HillClimb.java
@@ -29,70 +29,34 @@ public class HillClimb {
 
     public Tile[][] hillClimb(int iterations, Tile[][] grid) {
         System.out.println("\n\nStarting Hill Climb");
-        int originalGridValue = evaluate(grid);
-
-
-//        System.out.println(temp);
-//        System.out.println(grid);
 
         for(int i = 0; i < iterations; i++) {
-            Tile[][] temp  = new Tile[grid.length][];
-            temp = BFS.clearMinimumDistance(grid);
-
-
+            int ogNum = evaluate(grid);
             int x = ThreadLocalRandom.current().nextInt(0, grid.length-1);
             int y = ThreadLocalRandom.current().nextInt(0, grid.length-1);
-            System.out.println("Switching original tile: "+grid[x][y].getxPosition()+", "+grid[x][y].getyPosition()+" ... value: "+grid[x][y].getNumber());
-            Tile randomTile = temp[x][y];
-            int initNum = randomTile.getNumber();
-            while (randomTile.getNumber() == initNum) {
-                randomTile.setNumber(ThreadLocalRandom.current().nextInt(1, grid.length-1));
+
+            int temp = grid[x][y].getNumber();
+            System.out.println("THIS IS WHAT TEMP IS: " + temp);
+
+            while(grid[x][y].getNumber() == temp) {
+                grid[x][y].setNumber(ThreadLocalRandom.current().nextInt(1, grid.length-1));
             }
-
-            System.out.println("To random valued tile: "+randomTile.getxPosition()+", "+randomTile.getyPosition()+" ... value: "+randomTile.getNumber());
-            temp[x][y]=randomTile;
-
-            temp = BFS.clearMinimumDistance(temp);
-            temp = BFS.breadthFirstSearch(temp);
-
-            // print regular board
-            for(int k = 0; k <grid.length; k++){
-                for(int j = 0; j<grid.length; j++){
-                    System.out.print(grid[k][j]+"\t");
-                }
+            System.out.println("NEW GRID NUM WENT FROM TEMP: "+ temp + "to " + grid[x][y].getNumber());
+            BFS.clearMinimumDistance(grid);
+            BFS.breadthFirstSearch(grid);
+            int newNum = evaluate(grid);
+            if(newNum >= ogNum) {
+                System.out.println("FOUND A NEW NUM IS BIGGER: " + newNum + " vs OLD NUM: " + ogNum);
+                continue;
+            } else {
                 System.out.println();
+                System.out.println("FOUND A NEW NUM IS SMALLER: " + newNum + " vs OLD NUM: " + ogNum);
+                System.out.println();
+                grid[x][y].setNumber(temp);
+                System.out.println("TEMP WINS HERE: " + grid[x][y].getNumber());
+                BFS.clearMinimumDistance(grid);
+                BFS.breadthFirstSearch(grid);
             }
-
-            System.out.println("orig "+originalGridValue);
-            System.out.println("Eval of temp: "+evaluate(temp));
-
-            grid = BFS.clearMinimumDistance(grid);
-            temp = BFS.clearMinimumDistance(temp);
-            grid = BFS.breadthFirstSearch(grid);
-            temp = BFS.breadthFirstSearch(temp);
-            if (evaluate(temp) > originalGridValue) {
-                System.out.println("Random tile change made the board longer to solve!");
-                originalGridValue = evaluate(temp);
-
-                for (int r = 0; r < temp.length; r++) {
-                    grid[r] = temp[r].clone();
-                }
-                // print regular board
-                for(int k = 0; k <grid.length; k++){
-                    for(int j = 0; j<grid.length; j++){
-                        System.out.print(grid[k][j]+"\t");
-                    }
-                    System.out.println();
-                }
-                //grid = b.BFS(grid);
-                //grid = temp;
-            } else if (evaluate(temp) == originalGridValue) {
-                System.out.println("Random tile change made the board the same!");
-            }  else{
-                System.out.println("Random tile change made it simpler.");
-            }
-            System.out.println();
-//            System.out.print(evaluate(grid));
         }
 
         System.out.println("returning val: "+evaluate(grid));
